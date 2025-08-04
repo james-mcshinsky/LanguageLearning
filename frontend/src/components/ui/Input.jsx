f61d70ff05a8940d7320143fbd41397ad1decfc4
james-mcshinsky <JAMES.MCSHINSKY@GMAIL.COM>
2025-08-04 15:43:05 -0600

    feat: add focus glow style to Input

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function Input({ as = 'input', className = '', ...props }) {
   const Component = as;
-  const base = 'border rounded p-xs focus:outline focus:outline-2 focus:outline-offset-2';
+  const base =
+    'border rounded p-xs focus:outline focus:outline-2 focus:outline-offset-2 focus:shadow-[0_0_0_3px_rgba(255,191,50,0.3)]';
   return <Component className={`${base} ${className}`} {...props} />;
 }
